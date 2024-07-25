c47469cd11a00b6f5d47f7496e0e2a222eb0180b
nguyendes <nguyendes1410@gmail.com>
2024-07-25 11:58:57 +0700

    first commit

---
+++ src/main/java/com/example/Domain/Entities/Order_Details.java
@@ -0,0 +1,26 @@
+package com.example.Domain.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Order_Details {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Orders orders;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+}
